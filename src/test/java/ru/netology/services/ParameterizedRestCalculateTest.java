bfd0215efa9f52ccf2c29aea4b15c76692742ef4
Irina <yadrio89@gmail.com>
2024-03-11 23:09:38 +0500

    Исправление CamelCase

--- src/test/java/ru/netology/services/ParameterizedRestCalculateTest.java
+++ src/test/java/ru/netology/services/ParameterizedRestCalculateTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
-public class parameterizedrestcalculatetest {
+public class ParameterizedRestCalculateTest {
 
     @ParameterizedTest
     @CsvFileSource(files = "src/test/resources/rest.csv")
